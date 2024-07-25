2338f074395ca4d327f435a56a640afd83eae1ed
ash <ayushcool1275@gmail.com>
2024-07-25 19:21:15 +0530

    07252024

---
+++ js/functions/Impure_Functions.js
@@ -0,0 +1,10 @@
+let c = 1;
+
+function sum(a,b) {
+    console.log(a+b+c);
+    c++;
+}
+
+sum(4, 5);
+sum(4, 5);
+sum(4, 5);
---
+++ js/functions/Pure_Functions.js
@@ -0,0 +1,9 @@
+function sum(a,b) {
+    let c = 1;
+    console.log(a+b+c);
+    c++;
+}
+
+sum(4, 5);
+sum(4, 5);
+sum(4, 5);
